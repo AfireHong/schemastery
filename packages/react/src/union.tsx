4c2b6325ea115f7182da20bc121da283d676b016
yijie4188 <yijie4188@gmail.com>
2023-03-14 11:41:06 +0800

    feat(react): union type

---
+++ packages/react/src/union.tsx
@@ -0,0 +1,11 @@
+import { ControllerProps } from './controller'
+
+export interface UnionProps extends ControllerProps {
+}
+
+export function Union({
+  schema,
+  ...rest
+}: UnionProps) {
+  return <></>
+}
